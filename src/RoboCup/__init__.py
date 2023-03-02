a9082b98e3e5455b7c1158ed34ff61fa643a89bb
denyahnov <den.yakhnov@gmail.com>
2023-03-02 20:23:13 +1100

    Update __init__.py

--- src/RoboCup/__init__.py
+++ src/RoboCup/__init__.py
@@ -6,6 +6,7 @@ from ev3dev2.sound import Sound
 from ev3dev2.led import Leds
 from ev3dev2.motor import SpeedPercent
 
+from smbus import SMBus
 from math import sin, cos, tan
 
 # ev3dev API Reference:
@@ -16,8 +17,11 @@ from math import sin, cos, tan
 
 
 class Driver:
-	IR_SENSOR = "ht-nxt-ir-seek-v2"
-	COMPASS_SENSOR = "ht-nxt-compass"
+	IR = "ht-nxt-ir-seek-v2"
+	COMPASS = "ht-nxt-compass"
+
+class Address:
+	IR_360 = "ev3:in1:i2c8"
 
 def Unpack(values) -> list:
 	for item in values:
@@ -69,6 +73,30 @@ class PID_Controller():
 		d = error
 
 		return p + (self.ki * i) + (self.kd * d)
+
+class IRSeeker360():
+	def __init__(self,port:int):
+		self.port = port if type(port) == int else int(port[-1])
+
+		self.i2c_address = 0x08
+
+		self.create_bus()
+
+	def read(self):
+		# Read infrared sensor bin data from all 12 sensors
+		raw_ir = [self.bus.read_i2c_block_data(self.i2c_address, i, 2) for i in range(12)]
+
+		# Fix invalid read data
+		all_angles = [value for value in raw_ir[0] if 0 <= value <= 12]
+
+		# return Angle, Strength
+		return max(set(all_angles), key = all_angles.count), max(set(raw_ir[1]), key = raw_ir[1].count)
+
+	def create_bus(self):
+		self.bus = SMBus(self.port + 0x2)
+
+	def close(self):
+		self.bus.close()
 
 class FilteredSensor():
 	def __init__(self,difference: int = 200, outliers: int = 15) -> None:
